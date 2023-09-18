4d89b7ad22251331107cb25e952a46958d2f8923
JingtongYang <77351683+JingtongYang@users.noreply.github.com>
2023-09-18 17:45:56 -0600

    Update lab1_404.py

--- lab1_404.py
+++ lab1_404.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
  
-resp = requests.get("http://google.com")
+resp = requests.get("https://github.com/JingtongYang/lab1-cmput404/edit/main/lab1_404.py")
 print(resp.text)
 
